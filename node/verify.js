efb37854a6aae8b7ee13f46ca174efdb28637b1e
Forrest L Norvell <forrest@newrelic.com>
2013-05-28 01:45:15 -0700

    Minor performance tweaks.

--- node/verify.js
+++ node/verify.js
@@ -1,16 +1,12 @@
 'use strict';
 
-var createHash = require('crypto').createHash;
+var Hash = require('crypto').Hash;
 
 function verify(input, nonce) {
   if (!input) return;
 
-  var sha256 = createHash('sha256');
-  sha256.update(input);
-  sha256.update(nonce);
-
-  var hashed = sha256.digest('hex');
-  if (hashed.slice(-2) === '00') return hashed;
+  var hashed = new Hash('sha256').update(input).update(nonce).digest('hex');
+  if (hashed.substr(62, 2) === '00') return hashed;
 }
 
 module.exports = verify;
