080bff0f355d20ee5314238831d3b2cac7022c6e
Ralph Bodenner <rkbodenner+github@gmail.com>
2013-05-31 22:46:38 -0700

    Move file to reflect naming convention

--- rust/ralph-libuv-tasks/server.rs
+++ rust/ralph-libuv-tasks/server.rs
@@ -1,9 +1,8 @@
 extern mod std;
 
-fn main() {
-  let ip = std::net_ip::v4::parse_addr("127.0.0.1");
-  let task = std::uv_global_loop::get();
+use sha256;
 
+fn main() {
   type ConnectMsg = (std::net::tcp::TcpNewConnection, comm::SharedChan<Option<std::net::tcp::TcpErrData>>);
   
   let (port, chan): (Port<ConnectMsg>, Chan<ConnectMsg>) = stream();
@@ -16,13 +15,16 @@ fn main() {
         info!("Accepted connection");
         let socket_buf = std::net::tcp::socket_buf(socket);
         let socket_write = @socket_buf as @io::WriterUtil;
-        socket_write.write_line("dood");
+        socket_write.write_line("ok");
       },
       Err(_) => {
         info!("Failed to accept connection");
       }
     }
   }
+
+  let ip = std::net_ip::v4::parse_addr("127.0.0.1");
+  let task = std::uv_global_loop::get();
 
   match std::net::tcp::listen(ip, 1337, 1, &task,
     |_| {
