f3b2dd73f899fb869d7c4434af14ee1f4cfccb9a
Ralph Bodenner <rkbodenner+github@gmail.com>
2013-06-04 08:30:09 -0700

    Shell out to get SHA-256. Broken in compile step, tho.

--- rust/ralph-libuv-tasks/server.rs
+++ rust/ralph-libuv-tasks/server.rs
@@ -1,6 +1,25 @@
 extern mod std;
+pub use core::libc::*;
 
 fn sha256(msg: &str) -> ~str {
+  unsafe {
+    let s = ~"echo " + msg + " | shasum -a 256 | cut -f 1 -d ' '";
+    info!(s);
+    let file = str::as_c_str(s, { |cmd|
+      str::as_c_str("r", { |mode|
+        popen(cmd, mode)
+      })
+    });
+    let freader = (@file as @Reader);
+    let mut bytes : ~[u8] = ~[];
+    loop {
+      let byte : int = freader.read_byte();
+      if freader.eof() { break }
+      info!(fmt!("%?", byte as char));
+      vec::push(bytes, byte as u8);
+    }
+    info!(fmt!("%?", bytes));
+  }
   ~""
 }
 
@@ -24,6 +43,7 @@ fn main() {
           info!(fmt!("Read %s", input_str));
 
           let nonce = ~"5a";
+          info!(sha256(input_str + nonce));
 
           socket.write(str::to_bytes(input_str + ":" + nonce));
         },
